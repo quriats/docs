1a49be79a01d8953639d2f867a1496b61fbe7c08
Luca Pattocchio <pattocchioluca@gmail.com>
2025-06-06 19:29:40 +0200

    Enhance Starlight integration with custom configurations and theme selector
    - Updated Astro configuration for Starlight integration with new title, description, favicon, and logo.
    - Added custom CSS for theme management and styling.
    - Introduced a theme selection component for user-friendly theme switching.
    - Updated image paths in documentation to reflect new structure.
    - Removed unused favicon SVG file and added new favicon.ico.
    - Added Houston image and logo assets.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,42 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "Quria",
+      description:
+        "A user-friendly Destiny 2 API Wrapper written with TypeScript and approved by Oryx.",
+      favicon: "/favicon.ico",
+      logo: {
+        src: "./src/assets/images/logo.png",
+      },
+      social: [
+        {
+          icon: "github",
+          label: "Quria on GitHub",
+          href: "https://github.com/quriats/quria",
+        },
+      ],
+      customCss: ["./src/assets/styles/styles.css"],
+      components: {
+        ThemeSelect: "./src/components/ThemeSelect.astro",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
 });
